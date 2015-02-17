038f7776c486bffeaa819ca69257ea3e8cb471e8
1000ch <shogo.sensui@gmail.com>
2015-02-17 18:25:43 +0900

    delete cache on message

--- service-worker.js
+++ service-worker.js
@@ -6,7 +6,6 @@ self.addEventListener('install', function (e) {
 
   e.waitUntil(
     caches.open(CACHE_KEY).then(function (cache) {
-
       return cache.addAll([
         'index.html',
         'vulcanized.html',
@@ -21,29 +20,39 @@ self.addEventListener('install', function (e) {
   );
 });
 
+self.addEventListener('message', function (e) {
+  
+  switch (e.data.indication) {
+    case 'clear-cache':
+      e.waitUntil(
+        caches.keys().then(function (cacheNames) {
+          return Promise.all(
+            cacheNames.map(function (cacheName) {
+              return caches.delete(cacheName)
+            })
+          )
+        })
+      );
+      break;
+    default:
+      break;
+  }
+});
+
 self.addEventListener('fetch', function (e) {
   
   e.respondWith(
     caches.open(CACHE_KEY).then(function (cache) {
-
       return cache.match(e.request).then(function (response) {
         if (response) {
-
           console.log('cache was found', response);
 
-          // e.requestに対するキャッシュが見つかったのでそれを返却
           return response;
         } else {
-
           console.log('cache was not found', e.request);
-
-          // キャッシュが見つからなかったので取得
+          
           fetch(e.request.clone()).then(function (response) {
-
-            // 取得したリソースをキャッシュに登録
             cache.put(e.request, response.clone());
-
-            // 取得したリソースを返却
             return response;
           });
         }
