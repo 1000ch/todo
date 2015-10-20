0fbcea6932df0981ae0f54b2012c5e5288edc1b3
1000ch <shogo.sensui@gmail.com>
2015-10-20 22:02:02 +0900

    update polymer

--- service-worker.js
+++ service-worker.js
@@ -1,36 +1,27 @@
-importScripts('bower_components/cache-polyfill/dist/serviceworker-cache-polyfill.js');
-
 var CACHE_KEY = 'todo-cache-v1';
 
-self.addEventListener('install', function (e) {
+self.addEventListener('install', e => {
 
   e.waitUntil(
-    caches.open(CACHE_KEY).then(function (cache) {
+    caches.open(CACHE_KEY).then(cache => {
       return cache.addAll([
         'index.html',
         'vulcanized.html',
-        'bower_components/cache-polyfill/dist/serviceworker-cache-polyfill.js',
-        'bower_components/rsvp/rsvp.min.js',
-        'bower_components/webcomponentsjs/webcomponents.min.js',
-        'bower_components/polymer/polymer.js'
+        'bower_components/node-uuid/uuid.js',
+        'bower_components/promise-polyfill/Promise.js',
+        'bower_components/webcomponentsjs/webcomponents.min.js'
       ]);
-    }, function (e) {
-      console.log(e);
-    })
+    }).catch(e => console.log(e))
   );
 });
 
-self.addEventListener('message', function (e) {
-  
+self.addEventListener('message', e => {
+
   switch (e.data.indication) {
     case 'clear-cache':
       e.waitUntil(
-        caches.keys().then(function (cacheNames) {
-          return Promise.all(
-            cacheNames.map(function (cacheName) {
-              return caches.delete(cacheName)
-            })
-          )
+        caches.keys().then(cacheNames => {
+          return Promise.all(cacheNames.map(cacheName => caches.delete(cacheName)))
         })
       );
       break;
@@ -39,23 +30,15 @@ self.addEventListener('message', function (e) {
   }
 });
 
-self.addEventListener('fetch', function (e) {
-  
+self.addEventListener('fetch', e => {
+
   e.respondWith(
-    caches.open(CACHE_KEY).then(function (cache) {
-      return cache.match(e.request).then(function (response) {
-        if (response) {
-          console.log('cache was found', response);
-
+    caches.open(CACHE_KEY).then(cache => {
+      return cache.match(e.request).then(response => {
+        return response || fetch(e.request.clone()).then(response => {
+          cache.put(e.request, response.clone());
           return response;
-        } else {
-          console.log('cache was not found', e.request);
-
-          fetch(e.request.clone()).then(function (response) {
-            cache.put(e.request, response.clone());
-            return response;
-          });
-        }
+        });
       });
     })
   );
